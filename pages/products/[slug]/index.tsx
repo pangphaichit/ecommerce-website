a5ec74736c561a8a2ddd6c8d5dd0db82aa0c763c
pangphaichit <pg_grj@hotmail.com>
2025-05-14 17:51:25 +0700

    feat: add product slug page for future cart functionality

---
+++ pages/products/[slug]/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ProductSlugPage from "@/components/pages/ProductSlugPage";
+
+const Product: React.FC = () => {
+  return (
+    <>
+      <ProductSlugPage />
+    </>
+  );
+};
+
+export default Product;
